e7ef76b22d9f277334dab819bb2104190b946f48
JrCs <90z7oey02@sneakemail.com>
2016-01-26 18:22:50 +0100

    Improve init files

--- custom.el
+++ custom.el
@@ -4,15 +4,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(current-language-environment "French")
  '(custom-safe-themes
    (quote
-	("bfb9e8917a7c90449ad038e91581d7482f0d48ae6dabaaf4455c68d6ec11421a" "6e19e9c7389e2b67a2b7e85669621d632a40fb606a13d62e8e2ddede76ff7fe4" "b716dd657b254c9ae88442c96f65fa26dcc68d82375dabe1db0457bf1ac8a17d" "569ac95e6d5b56c366b7285f02bebd0e020bcee8c8f94a509658ed3c4bd5fe6f" "0be57c280e7fc2d3e6bfe92d36c5b7b8ac99e3d3f5e2c6f7d9739b3430548fcc" default)))
+	("6afbd77066a77464bc3af15331a1e978938be49d06458d23a39ef469ec7bf716" default)))
  '(custom-theme-directory "~/.emacs.d/my-themes")
- '(desktop-files-not-to-save "^$")
+ '(delete-selection-mode t)
  '(display-time-24hr-format t)
  '(electric-pair-mode t)
+ '(font-use-system-font nil)
+ '(ibuffer-expert t)
+ '(ibuffer-show-empty-filter-groups nil)
  '(ido-enable-flex-matching t)
- '(line-number-mode t)
  '(recentf-mode t)
  '(save-place t nil (saveplace))
  '(savehist-mode t)
--- init.el
+++ init.el
@@ -13,6 +13,10 @@
 ;; Package configuration
 (require 'package)
 
+;; Marmalade configuration
+(add-to-list 'package-archives
+			 '("marmalade" . "http://marmalade-repo.org/packages/"))
+
 ;; MELPA configuration
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
@@ -25,6 +29,8 @@
 ;;;;;;;;;;;;;
 ;; Load Theme
 (load-theme 'jrcs)
+;(add-to-list 'default-frame-alist
+;             '(font . "Inconsolata-13"))
 
 ;; Customize Plus
 (require 'cus-edit+)
@@ -36,24 +42,62 @@
       (interactive)
       (switch-to-buffer (other-buffer (current-buffer) 1)))
 
+;;;;;;;;;;;
+;; Encoding
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+
 ;;;;;;
 ;; IDO
 (require 'ido)
+(require 'ido-completing-read+)
 (ido-mode t)
 (ido-everywhere 1)
-
-(require 'ido-completing-read+)
 
 ;;;;;;;;;;
 ;; IBuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
+(setq ibuffer-saved-filter-groups
+      '(("home"
+         ("Perl" (mode . perl-mode))
+         ("GO"   (mode . go-mode))
+         ("emacs-config" (or (filename . ".emacs.d")
+                             (filename . "emacs-config")))
+         ("Help" (or (name . "\*Help\*")
+                     (name . "\*Apropos\*")
+                     (name . "\*info\*"))))))
+(add-hook 'ibuffer-mode-hook
+          '(lambda ()
+             (ibuffer-switch-to-saved-filter-groups "home")))
+
+;;;;;;;;;;
+;; Shell
+
+;; Add a new face
+(defface sh-variable-used
+  '((((class color) (background dark))
+     (:foreground "violet"))
+    (((class color) (background light))
+     (:foreground "violet")))
+  "Face to show use of variable like $var or ${var}")
+;;; GNU emacs font-lock needs these...
+(defvar sh-variable-used-face 'sh-variable-used)
+
+(font-lock-add-keywords
+ 'sh-mode
+ '(; fontify variable use
+   ("[$][{]?\\([A-Za-z_][A-Za-z0-9_]*\\)[}]?" 1 sh-variable-used-face t)))
+
+;;(add-hook 'sh-mode-hook '(lambda () (message "hello")))
 
 ;;;;;;;;;;
 ;; Perl
-(autoload 'perltidy "perltidy-mode" nil t)
-(autoload 'perltidy-mode "perltidy-mode" nil t)
-(eval-after-load "cperl-mode"
-  '(define-key cperl-mode-map "\C-ct" 'perltidy))
+;(autoload 'perltidy "perltidy-mode" nil t)
+;(autoload 'perltidy-mode "perltidy-mode" nil t)
+(eval-after-load "perl-mode"
+  '(define-key perl-mode-map "\C-ct" 'perltidy-dwim-safe))
 ;(eval-after-load "cperl-mode"
 ;  '(progn (add-hook 'cperl-mode-hook 'perltidy-mode)
 ;		  (define-key cperl-mode-map "\C-ct" 'perltidy)))
@@ -62,6 +106,21 @@
 (autoload 'perlcritic        "perlcritic" "" t)
 (autoload 'perlcritic-region "perlcritic" "" t)
 (autoload 'perlcritic-mode   "perlcritic" "" t)
+
+;;;;;;;;;;;;
+;; RPM Spec
+(autoload 'rpm-spec-mode "rpm-spec-mode.el" "RPM spec mode." t)
+(setq auto-mode-alist
+      (append '(("\\.spec" . rpm-spec-mode)) auto-mode-alist))
+
+;;;;;;;;;;;;
+;; FlyCheck
+;(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;;;;;;;;;;;;
+;; GO
+(add-hook 'before-save-hook #'gofmt-before-save)
+(require 'golint)
 
 ;;;;;;;;;;;;;;;;;;;;;;;
 ;; Unix <-> Dos
@@ -88,19 +147,13 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Desktop Settings
-(desktop-save-mode 1)
-
-(global-set-key (kbd "<mouse-8>") 'mouse-yank-primary)
-(global-set-key (kbd "<M-mouse-8>") 'mouse-yank-secondary)
-(global-set-key (kbd "<C-down-mouse-8>") 'facemenu-menu)
-(global-set-key [vertical-line C-mouse-8] 'mouse-split-window-vertically)
-(global-set-key [vertical-scroll-bar C-mouse-8] 'mouse-split-window-vertically)
-(global-set-key [mode-line C-mouse-8] 'mouse-split-window-horizontally)
-(global-set-key [mode-line mouse-8] 'mouse-delete-other-windows)
+(desktop-save-mode nil)
 
 ;;;;;;;;;;;;;;;;;;;;;;
 ;; Custom Key Bindings
 (global-set-key (kbd "<C-tab>") 'switch-to-previous-buffer)
+
+(global-set-key (kbd "<f5>") 'font-lock-fontify-buffer)
 
 ;; undo
 (require 'redo+)
@@ -111,8 +164,11 @@
 (global-set-key (kbd "<C-f15>") 'redo)
 (global-set-key (kbd "<C-pause>") 'redo)
 
-
-
-
-
+(global-set-key (kbd "<mouse-8>") 'mouse-yank-primary)
+(global-set-key (kbd "<M-mouse-8>") 'mouse-yank-secondary)
+(global-set-key (kbd "<C-down-mouse-8>") 'facemenu-menu)
+(global-set-key [vertical-line C-mouse-8] 'mouse-split-window-vertically)
+(global-set-key [vertical-scroll-bar C-mouse-8] 'mouse-split-window-vertically)
+(global-set-key [mode-line C-mouse-8] 'mouse-split-window-horizontally)
+(global-set-key [mode-line mouse-8] 'mouse-delete-other-windows)
 
